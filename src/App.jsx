f14ad343e11a5ac2189de68904c8d0ddad3f488a
technoplay.aiservers <technoplay.aiservers@gmail.com>
2025-07-07 05:16:39 +0000

    Test: Added red background to App.jsx for styling debugging.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import InvestorPitch from './components/InvestorPitch';
 
 function App() {
   return (
-    <div className="App bg-gradient-to-br from-blue-100 to-purple-200 min-h-screen">
+    <div className="App bg-red-500 min-h-screen">
       <InvestorPitch />
     </div>
   );
